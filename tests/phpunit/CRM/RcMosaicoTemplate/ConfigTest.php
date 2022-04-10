c960d0759692d0be94fe676b4d4f50c3ce87dd79
Sandor Semsey <sandor@es-progress.hu>
2022-04-10 15:05:52 +0200

    tests

--- tests/phpunit/CRM/RcMosaicoTemplate/ConfigTest.php
+++ tests/phpunit/CRM/RcMosaicoTemplate/ConfigTest.php
@@ -3,7 +3,7 @@
 /**
  * This is a generic test class for the extension (implemented with PHPUnit).
  */
-class CRM_Remote_ConfigTest extends \PHPUnit\Framework\TestCase
+class CRM_RcMosaicoTemplate_ConfigTest extends \PHPUnit\Framework\TestCase
 {
 
   /**
@@ -29,14 +29,14 @@ class CRM_Remote_ConfigTest extends \PHPUnit\Framework\TestCase
     public function testTemplates()
     {
         $template = CRM_RcMosaicoTemplate_TemplateConfig::getTemplate();
-        self::assertTrue(array_key_exists("name", $template), "The config has to contain the name key.");
+        self::assertArrayHasKey("name", $template, "The config has to contain the name key.");
         self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_NAME, $template["name"], "The name has to be the expected value.");
-        self::assertTrue(array_key_exists("title", $template), "The config has to contain the title key.");
+        self::assertArrayHasKey("title", $template, "The config has to contain the title key.");
         self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_TITLE, $template["title"], "The title has to be the expected value.");
-        self::assertTrue(array_key_exists("path", $template), "The config has to contain the path key.");
+        self::assertArrayHasKey("path", $template, "The config has to contain the path key.");
         self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_PATH, $template["path"], "The path has to be the expected value.");
-        self::assertTrue(array_key_exists("thumbnail", $template), "The config has to contain the thumbnail key.");
+        self::assertArrayHasKey("thumbnail", $template, "The config has to contain the thumbnail key.");
         self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_THUMBNAIL, $template["thumbnail"], "The thumbnail has to be the expected value.");
-        self::assertEquals(4, count($template), "The template has to contain only the name, title, path, thumbnail items.");
+        self::assertCount(4, $template, "The template has to contain only the name, title, path, thumbnail items.");
     }
 }
