6726d5821a5ce76ce05ed89a55611801b5fc472c
Sandor Semsey <sandor@es-progress.hu>
2023-03-15 23:42:22 +0100

    code cleanup

--- tests/phpunit/CRM/RcMosaicoTemplate/ConfigTest.php
+++ tests/phpunit/CRM/RcMosaicoTemplate/ConfigTest.php
@@ -13,14 +13,14 @@ class CRM_RcMosaicoTemplate_ConfigTest extends HeadlessTestCase
     public function testTemplates()
     {
         $template = CRM_RcMosaicoTemplate_TemplateConfig::getTemplate();
-        self::assertArrayHasKey("name", $template, "The config has to contain the name key.");
-        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_NAME, $template["name"], "The name has to be the expected value.");
-        self::assertArrayHasKey("title", $template, "The config has to contain the title key.");
-        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_TITLE, $template["title"], "The title has to be the expected value.");
-        self::assertArrayHasKey("path", $template, "The config has to contain the path key.");
-        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_PATH, $template["path"], "The path has to be the expected value.");
-        self::assertArrayHasKey("thumbnail", $template, "The config has to contain the thumbnail key.");
-        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_THUMBNAIL, $template["thumbnail"], "The thumbnail has to be the expected value.");
-        self::assertCount(4, $template, "The template has to contain only the name, title, path, thumbnail items.");
+        self::assertArrayHasKey('name', $template, 'The config has to contain the name key.');
+        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_NAME, $template['name'], 'The name has to be the expected value.');
+        self::assertArrayHasKey('title', $template, 'The config has to contain the title key.');
+        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_TITLE, $template['title'], 'The title has to be the expected value.');
+        self::assertArrayHasKey('path', $template, 'The config has to contain the path key.');
+        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_PATH, $template['path'], 'The path has to be the expected value.');
+        self::assertArrayHasKey('thumbnail', $template, 'The config has to contain the thumbnail key.');
+        self::assertEquals(CRM_RcMosaicoTemplate_TemplateConfig::TEMPLATE_THUMBNAIL, $template['thumbnail'], 'The thumbnail has to be the expected value.');
+        self::assertCount(4, $template, 'The template has to contain only the name, title, path, thumbnail items.');
     }
 }
