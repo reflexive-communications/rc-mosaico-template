c38fc59a6092ea595958384ede140a9c3b88a855
Sandor Semsey <sandor@es-progress.hu>
2023-03-16 21:20:18 +0100

    add return type to hooks

--- rc_mosaico_template.php
+++ rc_mosaico_template.php
@@ -2,17 +2,14 @@
 
 require_once 'rc_mosaico_template.civix.php';
 
-// phpcs:disable
 use CRM_RcMosaicoTemplate_ExtensionUtil as E;
-
-// phpcs:enable
 
 /**
  * Implements hook_civicrm_config().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
  */
-function rc_mosaico_template_civicrm_config(&$config)
+function rc_mosaico_template_civicrm_config(&$config): void
 {
     _rc_mosaico_template_civix_civicrm_config($config);
 }
@@ -20,7 +17,7 @@ function rc_mosaico_template_civicrm_config(&$config)
 /**
  * Implements hook_civicrm_mosaicoBaseTemplates().
  */
-function rc_mosaico_template_civicrm_mosaicoBaseTemplates(&$templates)
+function rc_mosaico_template_civicrm_mosaicoBaseTemplates(&$templates): void
 {
     $newTemplate = CRM_RcMosaicoTemplate_TemplateConfig::getTemplate();
     $newTemplate['path'] = E::url($newTemplate['path']);
@@ -31,7 +28,7 @@ function rc_mosaico_template_civicrm_mosaicoBaseTemplates(&$templates)
 /**
  * Implements hook_civicrm_mosaicoConfig().
  */
-function rc_mosaico_template_civicrm_mosaicoConfig(&$config)
+function rc_mosaico_template_civicrm_mosaicoConfig(&$config): void
 {
     $config['tinymceConfig']['forced_root_block'] = 'div';
 }
